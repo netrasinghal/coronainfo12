8314849870b133e54010a0d70e43fe57fa8ff82b
netrasinghal <netrasinghal20@gmail.com>
2020-07-28 19:02:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-'use strict';
+const express = require('express')
+const mysql = require('mysql')
 
-const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
-const axios = require('axios');
+require('dotenv').config()
 
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+const app=express()
 
-exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  const agent = new WebhookClient({ request, response });
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+app.use(express.json())
 
-  function getSpreadsheetData(){
-    return axios.get('https://sheetdb.io/api/v1/39oraf7bripew');
-  }
-
-  function welcome(agent) {
-    const state = agent.parameters.state;
-    return getSpreadsheetData().then(res => {
-        res.data.map(datareq => {
-            if(datareq.State === state )
-            agent.add(`Okay! So the toal number of cases for $(state) are $(datareq.Cases). Active cases are $(datareq.Active). Recovered cases are $(datareq.Recovered). Total deaths are $(datareq.Deaths)`);
-        });
+app.post('/connectdb',(req,res)=>{
+    const state=req.queryResult.perameter.state
+    var msg=''
+    
+    const connection = mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'password',
+        database:'corona_data'
+    })
+    
+    connection.query('select * from corona_tb', (error,results,fields)=>{
+        resolve(results)
+    })
+    results.map(user=>{
+        if(state===user.State){
+            msg='Okay! So the toal number of cases for ${user.state} are ${user.Cases}. Active cases are ${user.Active}. Recovered cases are ${user.Recovered}. Total deaths are ${user.Deaths}'
+            return res.json({
+                fulfillmenttext: msg,
+                source: 'connectdb'
+            })
+        }
+        else
+        {
+            msg='Invalid Email ID!'
+            return res.json({
+                fulfillmenttext: msg,
+                source: 'connectdb'
+            })
+        }
     });
-  }
+    connection.end()
+})
 
-  function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-  }
-
-  let intentMap = new Map();
-  intentMap.set('Default Welcome Intent - state', welcome);
-  intentMap.set('Default Fallback Intent', fallback);
-  // intentMap.set('your intent name here', yourFunctionHandler);
-  // intentMap.set('your intent name here', googleAssistantHandler);
-  agent.handleRequest(intentMap);
-});
