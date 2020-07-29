9a6895ca1273c28e9bcc4e72e80e7cacc433e13e
netrasinghal <netrasinghal20@gmail.com>
2020-07-29 16:57:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,42 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment');
-const mysql = require('mysql');
+const express = require('express')
+const mysql = require('mysql')
 
-exports.chatbot = functions.https.onRequest((request, response) => {
-    const agent = new WebhookClient({ request, response });
+require('dotenv').config()
 
-function connectToDB(){
+const app=express()
+
+app.use(express.json())
+
+app.post('/chatbot',(req,res)=>{
+    const state=req.queryResult.perameter.state
+    var msg=''
+    
     const connection = mysql.createConnection({
         host:'localhost',
         user:'root',
         password:'password',
         database:'corona_data'
+    })
+    
+    connection.query('select * from corona_tb', (error,results,fields)=>{
+        resolve(results)
+    })
+    results.map(user=>{
+        if(state===user.State){
+            msg='Okay! So the toal number of cases for ${user.state} are ${user.Cases}. Active cases are ${user.Active}. Recovered cases are ${user.Recovered}. Total deaths are ${user.Deaths}'
+            return res.json({
+                fulfillmenttext: msg,
+                source: 'chatbot'
+            })
+        }
+        else
+        {
+            msg='Invalid State!'
+            return res.json({
+                fulfillmenttext: msg,
+                source: 'chatbot'
+            })
+        }
     });
-    return new Promise((resolve,reject)=>{
-        connection.connect();
-        resolve(connection);
-    });
-}
-
-function queryDB(connection){
-    return new Promise((resolve,reject)=>{
-        connection.query('select * from corona_tb', (error,results,fields)=>{
-            resolve(results);
-        });
-    });
-}
-
-function handleReadFromMySQL(agent){
-    const state = agent.parameters.state;
-    return connectToDB()
-    .then(connection=>{
-        return queryDB(connection)
-        .then(result=>{
-            console.log(result);
-            result.map(user=>{
-                if(state===user.State){
-                    agent.add('Okay! So the toal number of cases for ${user.state} are ${user.Cases}. Active cases are ${user.Active}. Recovered cases are ${user.Recovered}. Total deaths are ${user.Deaths}');
-                }
-            });
-            connection.end();
-        });
-    });
-}
-
-let intentMap = new Map();
-intentMap.set('Default Welcome Intent - state', handleReadFromMySQL);
-agent.handleRequest(intentMap);
-});
+    connection.end()
+})
